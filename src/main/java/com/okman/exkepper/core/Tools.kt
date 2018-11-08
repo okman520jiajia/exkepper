c1f53a82c23ae67c7e80ffd7d1745fce3a6cee3d
yuanbo <yuanbo@yuanbo.hsaecd.com>
2018-11-08 19:09:33 +0800

    add socket communicate

--- src/main/java/com/okman/exkepper/core/Tools.kt
+++ src/main/java/com/okman/exkepper/core/Tools.kt
@@ -1,8 +1,15 @@
 package com.okman.exkepper.core
 
+import sun.misc.BASE64Decoder
+import sun.misc.BASE64Encoder
 import java.math.BigDecimal
+import java.nio.charset.Charset
+import java.security.SecureRandom
 import java.text.SimpleDateFormat
 import java.util.*
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+
 
 fun List<BigDecimal>.sum() = this.fold(BigDecimal(0)) { acc, bigDecimal ->
     var b = acc + bigDecimal
@@ -11,13 +18,47 @@ fun List<BigDecimal>.sum() = this.fold(BigDecimal(0)) { acc, bigDecimal ->
 
 fun List<BigDecimal>.avg() = this.sum().divide(this.size.toBigDecimal(), 5, BigDecimal.ROUND_HALF_UP)
 
-fun Date.utcStr():String
-{
+fun Date.utcStr(): String {
     return utcFormat.format(this)
 }
 
 val utcFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 
-operator fun Date.minus(i: Int):String {
-    return utcFormat.format(this.time -i)
+operator fun Date.minus(i: Int): String {
+    return utcFormat.format(this.time - i)
+}
+
+val KEY_STR = "myKey"
+val CHARSETNAME = "UTF-8"
+val ALGORITHM = "DES"
+val base64encoder = BASE64Encoder()
+
+val key by lazy {
+    val generator = KeyGenerator.getInstance(ALGORITHM)
+    val secureRandom = SecureRandom.getInstance("SHA1PRNG")
+    secureRandom.setSeed(KEY_STR.toByteArray())
+    generator.init(secureRandom)
+    generator.generateKey()
+}
+
+fun String.desEncrypt(): String = try {
+    val bytes = this.toByteArray(Charset.forName(CHARSETNAME))
+    val cipher = Cipher.getInstance(ALGORITHM)
+    cipher.init(Cipher.ENCRYPT_MODE, key)
+    val doFinal = cipher.doFinal(bytes)
+    base64encoder.encode(doFinal)
+} catch (ex: java.lang.Exception) {
+    ""
+}
+
+
+fun String.desDecrypt(): String = try {
+    val base64decoder = BASE64Decoder()
+    val bytes = base64decoder.decodeBuffer(this)
+    val cipher = Cipher.getInstance(ALGORITHM)
+    cipher.init(Cipher.DECRYPT_MODE, key)
+    val doFinal = cipher.doFinal(bytes)
+    String(doFinal, Charset.forName(CHARSETNAME))
+} catch (e: Exception) {
+    ""
 }
--- src/main/java/com/okman/exkepper/mytest.kt
+++ src/main/java/com/okman/exkepper/mytest.kt
@@ -1,40 +1,96 @@
 package com.okman.exkepper
 
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.ServerSocket
+import java.net.Socket
+
 
 fun main(args: Array<String>) {
 
-    Thread{
+    KepperServer.startRun()
+    val reader = InputStreamReader(System.`in`)
+    while (true)
+    {
+        reader.forEachLine {
+            if(it.equals("show"))
+            {
+                println("客户数量:${KepperServer.clients.size}")
+            }
+        }
+    }
+}
+
+object KepperServer {
+
+    var clients = mutableListOf<ExClient>()
+
+    var serverSocket = ServerSocket(8888)
+
+    fun startRun() {
+        Thread {
+            println("服务器开启监听....")
+            while (true) {
+                val socket = serverSocket.accept()
+                val client = ExClient(socket)
+                clients.add(client)
+                client.startTalk()
+            }
+        }.start()
+    }
+}
+
+
+class ExClient(val socket: Socket) {
+
+    fun startTalk() {
+        println("${socket.remoteSocketAddress} 开启连接会话")
+        try {
+            process(socket.getInputStream())
+            println("客户结束会话")
+        } catch (ex: IOException) {
+            println("客服端异常断开")
+        } finally {
+            KepperServer.clients.remove(this)
+        }
+    }
+
+    val maxDataLen = 2048
+    val buffer = ByteArray(2)
+    val data = mutableListOf<Byte>()
+
+    fun process(inputStream: InputStream)
+    {
         while (true)
         {
-            produce()
-            Thread.sleep(3000)
+            val startSuffex0 = inputStream.read()
+            if(startSuffex0==-1)
+                return
+            val startSuffex1 = inputStream.read()
+            if(startSuffex1 == -1)
+                return
+            if(startSuffex0 == '\n'.toInt() && startSuffex1 == '\n'.toInt())
+            {
+                data.clear()
+                var datalen = 0
+                while (true)
+                {
+                    inputStream.read(buffer,0,2)
+                    datalen+=2
+                    if(buffer[0]!= '\n'.toByte() && buffer[1]=='\n'.toByte())
+                    {
+                        data.add(buffer[0])
+                        inputStream.read(buffer,0,1)
+                        datalen+=1
+                    }
+                    if((buffer[0] == '\n'.toByte() && buffer[1] == '\n'.toByte()) || datalen==maxDataLen) {
+                        break
+                    }
+                    data.addAll(buffer.asList())
+                }
+                println("userdata : ${String(data.toByteArray())}")
+            }
         }
-    }.start()
-
-    Thread{
-        while (true)
-        {
-            consume()
-        }
-    }.start()
-
+    }
 }
-
-var reslut : Int? = null
-
-private val lock = java.lang.Object()
-
-fun produce() = synchronized(lock) {
-    println("calc result")
-    reslut = 100
-    lock.notifyAll()
-}
-
-fun consume() = synchronized(lock) {
-    if(reslut==null)
-        lock.wait()
-    println("start work")
-    reslut = null
-}
-
-
