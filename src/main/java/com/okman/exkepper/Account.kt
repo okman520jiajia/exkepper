a5b8f54640a8b0974e57df65c452604e1c6fbb9b
HP <okman520jiajia@sina.com>
2018-11-08 20:48:57 +0800

    ccccc

--- src/main/java/com/okman/exkepper/Account.kt
+++ src/main/java/com/okman/exkepper/Account.kt
@@ -2,13 +2,45 @@ package com.okman.exkepper
 
 import java.math.BigDecimal
 
-data class Account(val userInfo: UserInfo)
-{
+class Account(val userInfo: UserInfo) : Runnable {
+
+    override fun run() {
+        println("账户<${userInfo.userName}>已开启自动托管模式开始自动运行...")
+        cancleOrder()
+        while (true) {
+            ticker?.let {
+                val target = it.filter { it -> it.instrument_id == "EOS-USD-181228" }
+                if (target?.size == 1) {
+                    with(target[0])
+                    {
+                        println("发现 ${instrument_id}开始计算买进价格")
+                        println("一阶买进挂单价 ${kAvage} * ${userInfo.userParam.buyRankIndex.firstRank} = ${kAvage * userInfo.userParam.buyRankIndex.firstRank}")
+                        println("二阶买进挂单价 ${kAvage} * ${userInfo.userParam.buyRankIndex.secRank} = ${kAvage * userInfo.userParam.buyRankIndex.secRank}")
+                        println("三阶买进挂单价 ${kAvage} * ${userInfo.userParam.buyRankIndex.thirdRank} = ${kAvage * userInfo.userParam.buyRankIndex.thirdRank}")
+                        println("四阶买进挂单价 ${kAvage} * ${userInfo.userParam.buyRankIndex.fourthRank} = ${kAvage * userInfo.userParam.buyRankIndex.fourthRank}")
+                    }
+                }
+            }
+            println("${userInfo.userName} 处理完毕")
+        }
+    }
+
+    private fun cancleOrder() {
+        println("取消上一未成交订单")
+    }
+
+    fun updateAccountInfo()= synchronized(accountLock){
+
+    }
+
     override fun toString() = "userName ${userInfo.userName} APIKey ${userInfo.apiKey} isauto ${userInfo.userParam.isDelegateByKepper}"
+
+    val accountLock = java.lang.Object()
+
 }
 
-data class UserInfo(val userName:String,val apiKey:String,val pwd:String,val userParam: UserParam)
+data class UserInfo(val userName: String, val apiKey: String, val pwd: String, val userParam: UserParam)
 
-data class UserParam(var isDelegateByKepper:Boolean,val buyRankIndex:RankIndex,val seelRankIndex:RankIndex)
+data class UserParam(var isDelegateByKepper: Boolean, val buyRankIndex: RankIndex, val seelRankIndex: RankIndex)
 
-data class RankIndex(var firstRank:BigDecimal,var secRank:BigDecimal,var thirdRank:BigDecimal,var fourthRank:BigDecimal)
+data class RankIndex(var firstRank: BigDecimal, var secRank: BigDecimal, var thirdRank: BigDecimal, var fourthRank: BigDecimal)
--- src/main/java/com/okman/exkepper/mytest.kt
+++ src/main/java/com/okman/exkepper/mytest.kt
@@ -1,40 +1,37 @@
 package com.okman.exkepper
 
+import com.okman.exkepper.db.getAllExUser
+import java.net.URI
+import java.net.URL
+import java.util.*
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+
 
 fun main(args: Array<String>) {
 
-    Thread{
-        while (true)
-        {
-            produce()
-            Thread.sleep(3000)
-        }
-    }.start()
+//    val threadPool = ThreadPoolExecutor(100, 200, 24, TimeUnit.HOURS, ArrayBlockingQueue<Runnable>(100))
+//    val accounts = getAllExUser()
+//    val timer = Timer()
+//
+//    timer.schedule(object : TimerTask() {
+//        override fun run() {
+//            calcKAvager()
+//            for (account in accounts) {
+//
+//                if (account.userInfo.userParam.isDelegateByKepper)
+//                    threadPool.execute(account)
+//
+//            }
+//        }
+//    }, 0, 3 * 60 * 1000)
 
-    Thread{
-        while (true)
-        {
-            consume()
-        }
-    }.start()
+    val url = URI("exkeeper://Login?username=yuanbo&passwd=123")
+    "exkeeper://AddAcount?"
+    println(url.scheme)
+    println(url.query)
 
-}
-
-var reslut : Int? = null
-
-private val lock = java.lang.Object()
-
-fun produce() = synchronized(lock) {
-    println("calc result")
-    reslut = 100
-    lock.notifyAll()
-}
-
-fun consume() = synchronized(lock) {
-    if(reslut==null)
-        lock.wait()
-    println("start work")
-    reslut = null
 }
 
 
